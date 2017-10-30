98e5af764d5be61960cbe4116557467a493f926e
dmlaziuk <dm.laziuk@gmail.com>
2017-10-30 15:40:53 +0300

    O an I modified

--- ruby/player.rb
+++ ruby/player.rb
@@ -71,22 +71,58 @@ class Player
   end
 
   def step_i
+    # str -- return command string
     str = ''
-    pos_y = @y_glass.map do |column|
-      y = column.index('    ')
-      y ? y : MAX_Y
+    # y_vert -- minimum y position in vertical mode
+    y_vert = MAX_Y - 1
+    # x_vert -- x position for minimum y
+    x_vert = 0
+
+    # find minimum level for element with width = 1 block
+    MAX_X.times do |x_pos|
+      y_pos = @y_glass[x_pos].reverse.index('*')
+      y1 = y_pos ? MAX_Y - y_pos : 0
+      if y1 < y_vert
+        y_vert = y1
+        x_vert = x_pos
+      end
     end
-    p pos_y
-    x = pos_y.each_with_index.min[1]
-    print 'min='
-    p x
+
+    # y_horiz -- minimum y position in horizontal mode
+    y_horiz = MAX_Y - 1
+    # x_horiz -- x position for minimum y
+    x_horiz = 0
+
+    # find minimum level for element with width = 4 block
+    (MAX_X - 3).times do |x_pos|
+      y_pos = Array.new(4)
+      4.times do |i|
+        y_i = @y_glass[x_pos + i].reverse.index('*')
+        y_pos[i] = y_i ? MAX_Y - y_i : 0
+      end
+      max_of_min, x_min = y_pos.each_with_index.max
+      if max_of_min < y_horiz
+        y_horiz = max_of_min
+        x_horiz = x_min
+      end
+    end
+
+    # forming command string
+    if y_vert < y_horiz
+      y = y_vert
+      x = x_vert
+    else
+      y = y_horiz
+      x = x_horiz
+    end
     if x < @x
       str << "left=#{@x - x}, "
     elsif x > @x
       str << "right=#{x - @x}, "
     end
+    str << 'rotate=1, ' if y_vert >= y_horiz
     str << 'drop'
-    puts "I: y=#{pos_y.min} x=#{x}, str=\"#{str}\""
+    puts "I: y=#{y} x=#{x}, str=\"#{str}\""
     str
   end
 
